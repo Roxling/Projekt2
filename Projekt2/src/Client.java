d7b2d9dbf8bb5ab0583a2aba6143fedb41bc1550
dat11epe <dat11epe@val-11.student.lth.se>
2014-02-18 16:30:16 +0100

    Client

--- Projekt2/src/Client.java
+++ Projekt2/src/Client.java
@@ -1,10 +1,143 @@
+import java.net.*;
+import java.io.*;
+
+import javax.net.ssl.*;
+import javax.security.cert.X509Certificate;
+
+import Server.Hash;
+
+import java.security.KeyStore;
+import java.security.cert.*;
+import java.io.Console;
+import java.util.Scanner;
 
 public class Client {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hej");
-		System.out.println("hejpådig");
+		
+		connectToHost(args);
+	   
+	}
+	
+	
+	
+	public static void connectToHost(String[] args){
+		
+		 String host = null;
+	        int port = -1;
+	        for (int i = 0; i < args.length; i++) {
+	            System.out.println("args[" + i + "] = " + args[i]);
+	        }
+	        if (args.length < 2) {
+	            System.out.println("USAGE: java client host port");
+	            System.exit(-1);
+	        }
+	        try { // get input parameters 
+	            host = args[0];
+	            port = Integer.parseInt(args[1]);
+	        } catch (IllegalArgumentException e) {
+	            System.out.println("USAGE: java client host port");
+	            System.exit(-1);
+	        }
+
+	        try { // set up a key manager for client authentication 
+	            SSLSocketFactory factory = null;
+	            try {
+	            	
+	                char[] password = "password".toCharArray();
+	                KeyStore ks = KeyStore.getInstance("JKS");
+	                KeyStore ts = KeyStore.getInstance("JKS");
+	                KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+	                TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+	                SSLContext ctx = SSLContext.getInstance("TLS");
+	                ks.load(new FileInputStream("certs/clientkeystore"), password);  // keystore password (storepass)
+					ts.load(new FileInputStream("certs/clienttruststore"), password); // truststore password (storepass);
+					kmf.init(ks, password); // user password (keypass)
+					tmf.init(ts); // keystore can be used as truststore here
+					ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+	                factory = ctx.getSocketFactory();
+	            } catch (Exception e) {
+	                throw new IOException(e.getMessage());
+	            }
+	            SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
+	            System.out.println("\nsocket before handshake:\n" + socket + "\n");
+
+	            /*
+	             * send http request
+	             *
+	             * See SSLSocketClient.java for more information about why
+	             * there is a forced handshake here when using PrintWriters.
+	             */
+	            socket.startHandshake();
+
+	            SSLSession session = socket.getSession();
+	            X509Certificate cert = (X509Certificate)session.getPeerCertificateChain()[0];
+	            String subject = cert.getSubjectDN().getName();
+	            System.out.println("certificate name (subject DN field) on certificate received from server:\n" + subject + "\n");
+	            System.out.println("socket after handshake:\n" + socket + "\n");
+	            System.out.println("secure connection established\n\n");
+
+	            
+	            login();
+	            askForMedRecords(socket);
+            
+	        	
+	    		
+	            socket.close();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		
+		
+	}
+	
+	public static void login(){
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Username: ");
+		String usrname = scan.nextLine();
+		
+		System.out.print("Password: ");
+		
+		/*
+		 Console cons = System.console();
+		    if (cons != null){
+		    	char[] password = cons.readPassword();
+			   // System.out.println(password);
+		    }else{
+		    	System.out.println("There is no console.");
+		    }
+		*/
+		
+		String passwd = scan.nextLine();
+		Hash.Crypt(passwd, "SALT");
+		
 	}
 
+	public static void askForMedRecords(SSLSocket socket) throws IOException{
+	
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		
+        String msg;
+		for (;;) {
+            System.out.print(">");
+            msg = read.readLine();
+            if (msg.equalsIgnoreCase("quit")) {
+			    break;
+			}
+            System.out.print("sending '" + msg + "' to server...");
+            out.println(msg);
+            out.flush();
+
+        }
+		
+		in.close();
+		out.close();
+		read.close();
+		
+	
+		
+	}
 }
